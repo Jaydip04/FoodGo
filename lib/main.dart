d99bbe65327ad35e39ff987844e008dcbed44b02
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-07-06 16:10:56 +0530

    project initialization

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:food_go/splash_screen.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyDOcisThBw-Vs3P-RhJrxWEDq8LJKI7AAQ",
+        appId: "1:301258823049:android:93d649d7083cfd37a63d58",
+        messagingSenderId: "301258823049",
+        projectId: "foodgo-a3df1",
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: splash_screen(),
+    );
+  }
+}
